66b87a3ba242102e84a044180ca8610f2a05aa41
Revchenko Dmitry <reuchankadzmitry@gmail.com>
2023-01-24 15:56:25 +0300

    feat: complete MessageList component

--- src/components/MessageList.tsx
+++ src/components/MessageList.tsx
@@ -1,13 +1,14 @@
 import React from "react";
 import List from "@mui/material/List";
 import Message from "./Message";
+import { IMessage } from "../models/message-model";
 
-export default function MessageList() {
+export default function MessageList({ messages }: { messages: IMessage[] }) {
   return (
     <List sx={{ width: "100%", bgcolor: "background.paper" }}>
-      <Message heading="heading" author="Dima Revchenko" id="1" />
-      <Message heading="heading" author="Dima Revchenko" id="2" />
-      <Message heading="heading" author="Dima Revchenko" id="3" />
+      {messages.map((item) => {
+        return <Message key={item._id} subject={item.subject} author={item.author} id={item._id} />;
+      })}
     </List>
   );
 }
