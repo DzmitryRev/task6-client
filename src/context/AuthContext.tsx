46c59180057cf2d78619fac40823792145d2a33c
Revchenko Dmitry <reuchankadzmitry@gmail.com>
2023-01-24 15:58:20 +0300

    refactor: add messages and setMessages to context

--- src/context/AuthContext.tsx
+++ src/context/AuthContext.tsx
@@ -1,15 +1,20 @@
 import { createContext } from "react";
+import { IMessage } from "../models/message-model";
 
 const AuthContext = createContext<{
   isAuth: boolean;
   name: string | null;
+  messages: IMessage[];
   setIsAuth: (value: boolean) => void;
   setName: (name: string) => void;
+  setMessages: (messages: IMessage[]) => void;
 }>({
   isAuth: false,
   name: null,
+  messages: [],
   setIsAuth: (value: boolean) => {},
-  setName: (name: string) => {}
+  setName: (name: string) => {},
+  setMessages: (messages: IMessage[]) => {},
 });
 
 export default AuthContext;
