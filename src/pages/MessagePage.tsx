9446203353dc89e107392e283cb5d02d78496d37
Revchenko Dmitry <reuchankadzmitry@gmail.com>
2023-01-24 16:02:35 +0300

    feat: complete impl. main logic messages and message pages (without socket)

--- src/pages/MessagePage.tsx
+++ src/pages/MessagePage.tsx
@@ -1,23 +1,41 @@
-import React from "react";
+import React, { useContext, useEffect } from "react";
 
 import { Box, Typography } from "@mui/material";
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
+import AuthContext from "../context/AuthContext";
 
 export default function MessagePage() {
+  const { isAuth, messages } = useContext(AuthContext);
+  const navigate = useNavigate();
+  useEffect(() => {
+    if (!isAuth) {
+      navigate("/");
+    }
+  }, [isAuth]);
+  
   const { id } = useParams();
 
-  console.log(id);
+  const message = messages.find((message) => message._id === id);
+
   return (
     <Box sx={{ width: "600px", p: 2 }}>
-      <Typography variant="h5" gutterBottom>
-        asasdasdasd
-      </Typography>
-      <Typography variant="h6" gutterBottom>
-        asdasdasd
-      </Typography>
-      <Typography variant="body1" gutterBottom>
-        sdasd
-      </Typography>
+      {message ? (
+        <>
+          <Typography variant="h5" gutterBottom>
+            {message?.subject}
+          </Typography>
+          <Typography variant="h6" gutterBottom>
+            {message?.author}
+          </Typography>
+          <Typography variant="body1" gutterBottom>
+            {message?.message}
+          </Typography>
+        </>
+      ) : (
+        <Typography variant="h6" gutterBottom>
+          Message not found
+        </Typography>
+      )}
     </Box>
   );
 }
--- src/pages/MessagesPage.tsx
+++ src/pages/MessagesPage.tsx
@@ -1,11 +1,29 @@
-import { Box } from "@mui/material";
-import React from "react";
+import React, { useContext, useEffect } from "react";
+import { Box, Typography } from "@mui/material";
 import MessageList from "../components/MessageList";
+import AuthContext from "../context/AuthContext";
+import { useNavigate } from "react-router-dom";
 
 export default function MessagesPage() {
+  const { isAuth, messages, name } = useContext(AuthContext);
+  const navigate = useNavigate();
+  useEffect(() => {
+    if (!isAuth) {
+      navigate("/");
+    }
+  }, [isAuth]);
+
+  // load messages every 5 sec
+
   return (
-    <Box sx={{ width: "600px" }}>
-      <MessageList />
+    <Box sx={{ width: "600px", p: 2 }}>
+      {messages.length ? (
+        <MessageList messages={messages} />
+      ) : (
+        <Typography variant="h6" gutterBottom>
+          You have't messages
+        </Typography>
+      )}
     </Box>
   );
 }
